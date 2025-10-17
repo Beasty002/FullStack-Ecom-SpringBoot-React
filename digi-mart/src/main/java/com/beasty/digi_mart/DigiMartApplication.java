6867f02317c53bf6d1744a52f875ef9997624dfb
Beasty002 <xasbin12@gmail.com>
2025-10-17 09:32:33 +0545

    feat: initialize repo

---
+++ digi-mart/src/main/java/com/beasty/digi_mart/DigiMartApplication.java
@@ -0,0 +1,13 @@
+package com.beasty.digi_mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiMartApplication.class, args);
+	}
+
+}
---
+++ digi-mart/src/test/java/com/beasty/digi_mart/DigiMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beasty.digi_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
